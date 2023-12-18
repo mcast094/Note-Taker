0fbe26bdaf8204782a8a2817aadfc74cb1a98623
Martin Castaneda <martincastaneda75@yahoo.com>
2023-12-17 18:40:10 -0800

    updated port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require("fs");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const dataBase = require('./db/db.json');
 const path = require('path');
